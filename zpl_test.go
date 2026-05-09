4732571da76bcd0e18ee617f37d56ef542c3b6af
vo1dw4lker <max.filonenko322@gmail.com>
2026-05-09 15:03:55 +0200

    test: update AddText test

--- zpl_test.go
+++ zpl_test.go
@@ -15,13 +15,27 @@ func TestLabel_AddCode128(t *testing.T) {
 }
 
 func TestLabel_AddText(t *testing.T) {
-	l := NewLabel()
-	l.AddText(5, 5, "A", 30, "hello world")
+	t.Run("basic", func(t *testing.T) {
+		l := NewLabel()
+		l.AddText(10, 20, "Test Label")
 
-	expected := "^XA\n^FO5,5^AA,30^FDhello world^FS\n^XZ\n"
-	result := l.String()
+		expected := "^XA\n^FO10,20^AA,N,15,15^FDTest Label^FS\n^XZ\n"
+		result := l.String()
 
-	if result != expected {
-		t.Errorf("AddText not matching expected output.\nExpected:\n%s\nGot:\n%s", expected, result)
-	}
+		if result != expected {
+			t.Errorf("AddText not matching expected output.\nExpected:\n%s\nGot:\n%s", expected, result)
+		}
+	})
+
+	t.Run("with options", func(t *testing.T) {
+		l := NewLabel()
+		l.AddText(10, 20, "Test Label", WithFont("0", 30, 20), WithTextOrientation(OrientationInverted), WithFieldBlock(200, 2, 0, "C"))
+
+		expected := "^XA\n^FO10,20^A0,I,30,20^FB200,2,0,C^FDTest Label^FS\n^XZ\n"
+		result := l.String()
+
+		if result != expected {
+			t.Errorf("AddText with options not matching expected output.\nExpected:\n%s\nGot:\n%s", expected, result)
+		}
+	})
 }
