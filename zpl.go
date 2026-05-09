40eefc0865cee6f1e89e41b944e0ac2c1c76449d
vo1dw4lker <max.filonenko322@gmail.com>
2026-05-09 15:21:14 +0200

    feat: config options for AddCode128

--- zpl.go
+++ zpl.go
@@ -119,13 +119,53 @@ func (l *Label) AddText(x, y int, text string, opts ...TextOption) *Label {
 	return l
 }
 
-func (l *Label) AddCode128(x, y int, height int, data string) *Label {
-	cmd := "^FO%d,%d^BCN,%d,N,N,Y,A^FD%s^FS\n"
-	l.builder.WriteString(fmt.Sprintf(
-		cmd,
-		x, y,
-		height,
-		data,
-	))
+type barcodeConfig struct {
+	height      int
+	orientation Orientation
+	printText   bool
+	textAbove   bool
+}
+
+type BarcodeOption func(*barcodeConfig)
+
+func WithBarcodeHeight(h int) BarcodeOption {
+	return func(c *barcodeConfig) {
+		c.height = h
+	}
+}
+
+func WithBarcodeOrientation(o Orientation) BarcodeOption {
+	return func(c *barcodeConfig) {
+		c.orientation = o
+	}
+}
+
+func WithBarcodeText(print bool, above bool) BarcodeOption {
+	return func(c *barcodeConfig) {
+		c.printText = print
+		c.textAbove = above
+	}
+}
+
+func (l *Label) AddCode128(x, y int, data string, opts ...BarcodeOption) *Label {
+	cfg := barcodeConfig{
+		height:      50,
+		orientation: OrientationNormal,
+		printText:   true,
+		textAbove:   false,
+	}
+	for _, opt := range opts {
+		opt(&cfg)
+	}
+	pText := "N"
+	if cfg.printText {
+		pText = "Y"
+	}
+	tAbove := "N"
+	if cfg.textAbove {
+		tAbove = "Y"
+	}
+	cmd := fmt.Sprintf("^FO%d,%d^BC%s,%d,%s,%s,N,N^FD%s^FS", x, y, string(cfg.orientation), cfg.height, pText, tAbove, data)
+	l.builder.WriteString(cmd + "\n")
 	return l
 }
