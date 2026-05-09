f852b8811d92c175e04fdd27a0d4403d89494f24
vo1dw4lker <max.filonenko322@gmail.com>
2026-05-09 17:10:22 +0200

    feat: add AddQRCode method

--- zpl.go
+++ zpl.go
@@ -169,3 +169,27 @@ func (l *Label) AddCode128(x, y int, data string, opts ...BarcodeOption) *Label 
 	l.builder.WriteString(cmd + "\n")
 	return l
 }
+
+type qrConfig struct {
+	magnification int // 1-10
+}
+
+type QROption func(*qrConfig)
+
+func WithQRMagnification(m int) QROption {
+	return func(c *qrConfig) {
+		c.magnification = m
+	}
+}
+
+func (l *Label) AddQRCode(x, y int, data string, opts ...QROption) *Label {
+	cfg := qrConfig{
+		magnification: 2,
+	}
+	for _, opt := range opts {
+		opt(&cfg)
+	}
+	cmd := fmt.Sprintf("^FO%d,%d^BQ%s,2,%d^FDQA,%s^FS", x, y, string(OrientationNormal), cfg.magnification, data)
+	l.builder.WriteString(cmd + "\n")
+	return l
+}
